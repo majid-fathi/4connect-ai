c8e9c029cb29968f303879a43f705a534717d068
majid-fathi <majid.fathi.iliya@gmail.com>
2020-05-25 10:52:36 +0430

    improve UI and AI

--- main.py
+++ main.py
@@ -1,18 +1,37 @@
 import os
+import sys
+import pygame
 from game import GameBoard
 from player import AIPlayer
 from player import Person
-import sys
-import pygame
 
+# colors
+RED = (255, 100, 100)
+BLUE = (100, 100, 255)
+GREEN = (100, 255, 100)
+WHITE = (255, 255, 255)
+BLACK = (10, 10, 10)
+GRAY = (100, 100, 100)
+GRAY_FILL = (80, 80, 80)
+LIGHT_RED = (255, 150, 150)
+LIGHT_BLUE = (150, 150, 255)
+LIGHT_GREEN = (150, 255, 150)
+GRAY_RED = (120, 100, 100)
+GRAY_BLUE = (100, 100, 120)
+# ------
 gameBoard = GameBoard()
 
-# ----------- py game
-pygame.init()
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
+
+ROW_COUNT = len(gameBoard.game_map)
+COLUMN_COUNT = len(gameBoard.game_map[0])
+
+pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-cycleRadius = int(((SCREEN_HEIGHT - SCREEN_HEIGHT / 6) / len(gameBoard.game_map)) / 2)
+pygame.display.set_caption("4 connect")
+clock = pygame.time.Clock()
+cycleRadius = int(((SCREEN_HEIGHT - SCREEN_HEIGHT / 6) / ROW_COUNT) / 2)
 arrow = pygame.image.load(os.path.join("images", "arrow.png")).convert()
 arrow = pygame.transform.scale(arrow, (cycleRadius * 2, cycleRadius * 2))
 arrowRed = pygame.image.load(os.path.join("images", "arrow-red.png")).convert()
@@ -20,104 +39,181 @@ arrowRed = pygame.transform.scale(arrowRed, (cycleRadius * 2, cycleRadius * 2))
 arrowBlue = pygame.image.load(os.path.join("images", "arrow-blue.png")).convert()
 arrowBlue = pygame.transform.scale(arrowBlue, (cycleRadius * 2, cycleRadius * 2))
 
-arrowsColor = [0] * len(gameBoard.game_map[0])
+finalFont = pygame.font.SysFont("monospace", 75)
+labelsFont = pygame.font.SysFont("monospace", 16)
 
-myFont = pygame.font.SysFont("monospace", 75)
+PLAYER_BUTTON_SIZE = (200, 30)
+PLAYER1_BUTTON_POS = ((COLUMN_COUNT * cycleRadius * 2) + 20, 50)
+PLAYER2_BUTTON_POS = ((COLUMN_COUNT * cycleRadius * 2) + 20, 100)
+START_BUTTON_SIZE = (100, 30)
+START_BUTTON_POS = (PLAYER1_BUTTON_POS[0] + 50, 250)
+
+game_running = False
+AI_FAKE_TIME = 5
+
+
+def draw_button(position, size, text, bright_color, color):
+    mouse_pos = pygame.mouse.get_pos()
+    x = position[0]
+    y = position[1]
+    width = size[0]
+    height = size[1]
+    if x < mouse_pos[0] < x + width and y < mouse_pos[1] < y + height and not game_running:
+        pygame.draw.rect(screen, bright_color, (x, y, width, height))
+    else:
+        pygame.draw.rect(screen, color, (x, y, width, height))
+
+    button_label = labelsFont.render(text, True, BLACK)
+    screen.blit(button_label, (x + 5, y + 5))
 
 
 def refresh_screen():
-    screen.fill((80, 80, 80))
-    for column in range(len(gameBoard.game_map[0])):
-        if arrowsColor[column] == 0:
-            screen.blit(arrow, (cycleRadius * column * 2, 10))
-        elif arrowsColor[column] == 1:
-            screen.blit(arrowBlue, (cycleRadius * column * 2, 10))
+    mouse_pos = pygame.mouse.get_pos()
+
+    screen.fill(GRAY_FILL)
+
+    mouse_column = int(mouse_pos[0] / (cycleRadius * 2))
+
+    #  draw Arrows
+    for column in range(COLUMN_COUNT):
+        if column == mouse_column and type(currentPlayer) == Person \
+                and game_running and not gameBoard.game_map[0][column]:
+            if currentPlayer == player1:
+                screen.blit(arrowBlue, (cycleRadius * column * 2, 10))
+            else:
+                screen.blit(arrowRed, (cycleRadius * column * 2, 10))
         else:
-            screen.blit(arrowRed, (cycleRadius * column * 2, 10))
+            screen.blit(arrow, (cycleRadius * column * 2, 10))
+
+    #  draw Cycles
     y = int(cycleRadius + SCREEN_HEIGHT / 6)
     for row in gameBoard.game_map:
         x = int(cycleRadius)
         for a in row:
             if a == 0:
-                pygame.draw.circle(screen, (100, 100, 100), (x, y), cycleRadius - 10)
+                pygame.draw.circle(screen, GRAY, (x, y), cycleRadius - 10)
             elif a == 1:
-                pygame.draw.circle(screen, (100, 100, 255), (x, y), cycleRadius - 10)
+                pygame.draw.circle(screen, BLUE, (x, y), cycleRadius - 10)
             else:
-                pygame.draw.circle(screen, (255, 100, 100), (x, y), cycleRadius - 10)
+                pygame.draw.circle(screen, RED, (x, y), cycleRadius - 10)
 
             x += 2 * cycleRadius
         y += 2 * cycleRadius
 
+    #  draw Buttons
+
+    if type(player1) == Person:
+        draw_button(PLAYER1_BUTTON_POS, PLAYER_BUTTON_SIZE, "Player 1: Person", LIGHT_BLUE, BLUE)
+    else:  # type(player1) == AI
+        draw_button(PLAYER1_BUTTON_POS, PLAYER_BUTTON_SIZE, "Player 1: AI Player", LIGHT_BLUE, BLUE)
+    if type(player2) == Person:
+        draw_button(PLAYER2_BUTTON_POS, PLAYER_BUTTON_SIZE, "Player 2: Person", LIGHT_RED, RED)
+    else:  # type(player2) == AI
+        draw_button(PLAYER2_BUTTON_POS, PLAYER_BUTTON_SIZE, "Player 2: AI Player", LIGHT_RED, RED)
+
+    if not game_running:
+        draw_button(START_BUTTON_POS, START_BUTTON_SIZE, "Start", LIGHT_GREEN, GREEN)
+
     pygame.display.flip()
 
 
-# -----------
-player1 = Person("player 1", 1)
-player2 = AIPlayer("AI player", 2)
+def my_func(position, size, mouse_pos):
+    if position[0] < mouse_pos[0] < position[0] + size[0] and position[1] < mouse_pos[1] < position[1] + size[1]:
+        return True
+    return False
 
-currentPlayer = player2
 
-refresh_screen()
-droppedCount = 0
-allSlots = len(gameBoard.game_map) * len(gameBoard.game_map[0])
-while droppedCount != allSlots:
-    dropped = False
-    if type(currentPlayer) == Person:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-            if event.type == pygame.MOUSEMOTION:
-                posX = event.pos[0]
-                if posX < len(gameBoard.game_map[0]) * cycleRadius * 2:
-                    arrowNum = int(posX / (cycleRadius * 2))
-                    for i in range(len(arrowsColor)):
-                        if arrowNum != i:
-                            arrowsColor[i] = 0
-                        elif gameBoard.game_map[0][i] == 0:
-                            arrowsColor[i] = currentPlayer.number
+player1 = Person(1)
+player2 = Person(2)
+
+currentPlayer = player1
+
+allSlots = COLUMN_COUNT * ROW_COUNT
+
+
+def run_game():
+    global currentPlayer
+    global gameBoard
+    global game_running
+    game_running = True
+    currentPlayer = player1
+    fake_wait = 0
+    dropped_count = 0
+    gameBoard = GameBoard()
+    while dropped_count != allSlots:
+        dropped = False
+        if type(currentPlayer) == Person:
+            for ev in pygame.event.get():
+                if ev.type == pygame.QUIT:
+                    sys.exit()
+
+                if ev.type == pygame.MOUSEBUTTONDOWN:
+                    if ev.pos[0] < COLUMN_COUNT * cycleRadius * 2:
+                        try:
+                            input_column = int(ev.pos[0] / (cycleRadius * 2))
+                            gameBoard.drop_piece(currentPlayer.number, input_column)
+                            dropped = True
+                        except Exception as exception:
+                            print(exception)
+
+        else:  # currentPlayer == AIPlayer
+            for ev in pygame.event.get():
+                if ev.type == pygame.QUIT:
+                    sys.exit()
+            fake_wait += 1
+            if fake_wait == AI_FAKE_TIME:
+                input_column = currentPlayer.get_drop_input(gameBoard.game_map)
+                gameBoard.drop_piece(currentPlayer.number, input_column)
+                dropped = True
+
+        refresh_screen()
+
+        if dropped:
+            fake_wait = 0
+            if gameBoard.check_win(currentPlayer.number):
+                break
+            if currentPlayer == player1:
+                currentPlayer = player2
+            else:
+                currentPlayer = player1
+            dropped_count += 1
+
+        clock.tick(30)
+
+    if dropped_count == allSlots:
+        label = finalFont.render("Game draw", True, WHITE)
+    elif currentPlayer == player1:
+        label = finalFont.render("Player 1 won !!", True, LIGHT_BLUE)
+    else:
+        label = finalFont.render("Player 2 won !!", True, LIGHT_RED)
+
+    screen.blit(label, (40, ROW_COUNT * cycleRadius))
+    pygame.display.update()
+    pygame.time.wait(1000)
+    game_running = False
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if my_func(PLAYER1_BUTTON_POS, PLAYER_BUTTON_SIZE, event.pos):
+                #  player 1 button pressed
+                if type(player1) == Person:
+                    player1 = AIPlayer(1)
                 else:
-                    arrowsColor = [0] * len(gameBoard.game_map[0])
-
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                clickX = event.pos[0]
-                if clickX < len(gameBoard.game_map[0]) * cycleRadius * 2:
-                    try:
-                        colInput = int(clickX / (cycleRadius * 2))
-                        gameBoard.drop_piece(currentPlayer.number, colInput)
-                        arrowsColor = [0] * len(gameBoard.game_map[0])
-                        if currentPlayer == player1 and type(player2) != AIPlayer:
-                            arrowsColor[colInput] = player2.number
-                        else:
-                            arrowsColor[colInput] = player1.number
-                        dropped = True
-                    except Exception as exception:
-                        print(exception)
-
-    else:  # currentPlayer == AIPlayer
-        col = currentPlayer.get_drop_input(gameBoard.game_map)
-        gameBoard.drop_piece(currentPlayer.number, col)
-        dropped = True
+                    player1 = Person(1)
+            elif my_func(PLAYER2_BUTTON_POS, PLAYER_BUTTON_SIZE, event.pos):
+                #  player 2 button pressed
+                if type(player2) == Person:
+                    player2 = AIPlayer(2)
+                else:
+                    player2 = Person(2)
+            elif my_func(START_BUTTON_POS, START_BUTTON_SIZE, event.pos):
+                #  start button pressed
+                run_game()
 
     refresh_screen()
-
-    if dropped:
-        if gameBoard.check_win(currentPlayer.number):
-            break
-        if currentPlayer == player1:
-            currentPlayer = player2
-        else:
-            currentPlayer = player1
-        droppedCount += 1
-
-if droppedCount == allSlots:
-    label = myFont.render("Game draw", 1, (255, 255, 255))
-elif currentPlayer == player1:
-    label = myFont.render("Player 1 won !!", 1, (100, 100, 255))
-else:
-    label = myFont.render("Player 2 won !!", 1, (255, 100, 100))
-
-screen.blit(label, (40, len(gameBoard.game_map) * cycleRadius))
-pygame.display.update()
-pygame.time.wait(2000)
-gameBoard.print_map()
-print("GG WP winner : {}".format(currentPlayer.name))
+    clock.tick(30)
--- player.py
+++ player.py
@@ -1,7 +1,6 @@
 from game import GameBoard
 class Player:
-    def __init__(self, name, number):
-        self.name = name
+    def __init__(self, number):
         self.number = number
 
     def get_drop_input(self, game_map):
@@ -9,63 +8,89 @@ class Player:
 
 
 class Person(Player):
-    def __init__(self, name, number):
-        super().__init__(name, number)
+    def __init__(self, number):
+        super().__init__(number)
 
     def get_drop_input(self, game_map):
         raise Exception("get_drop_input() called from Person class")
 
 
 class AIPlayer(Player):
-    def __init__(self, name, number):
-        super().__init__(name, number)
+    def __init__(self, number):
+        super().__init__(number)
 
 
     def check_Opponentـwin(self,game_map,player_num) :
 
 
-        #vertical                   
+
+
+
+        #vertical
         for x in range(len(game_map[0])):
-            
+
             for y in range(len(game_map) -2):
                 if game_map[y][x] != player_num:
+
                     continue
                 elif game_map[y+1][x] == player_num and game_map[y+2][x] == player_num :
-                    print("ddddddddddddddd")
-                    
-                    if  y-1>0:
+
+
+
+                    if  y-1>-1:
+
+
                         if game_map[y-1][x]==0:
+
                             return x
-                       
-                        
 
 
-                        
+
+
+
 
 
 
         #horizantal
 
         for x in range(len(game_map)):
-            
+
             for y in range(len(game_map[0]) -3):
+
                 if game_map[x][y] != player_num:
                     continue
                 elif game_map[x][y+1] == player_num and game_map[x][y+2] == player_num :
                     if y-1>-1 :
                         if x==5 and game_map[x][y-1] == 0:
                             return y-1
-                        
+
                         elif x<5 and game_map[x+1][y-1]!=0 and game_map[x][y-1]==0:
                             return y-1
                     if y+3<7:
 
                         if x==5 and game_map[x][y+3] == 0:
                             return y+3
-                        
-                        elif x<5 and game_map[x+1][y+3]!=0 and game_map[x][y+3]==0:
-                            return y+3                        
 
+                        elif x<5 and game_map[x+1][y+3]!=0 and game_map[x][y+3]==0:
+                            return y+3
+                elif game_map[x][y+1]==0 and game_map[x][y+2]==player_num and game_map[x][y+3]:
+                    if x==5:
+
+                        return y+1
+
+                    else :
+                        if game_map[x+1][y+1]!=0:
+                            return y+1
+                elif game_map[x][y+1]==player_num and game_map[x][y+2]==0 and game_map[x][y+3]:
+                    if x==5:
+
+                        return y+2
+
+                    else :
+                        print(x)
+                        print(y)
+                        if game_map[x+1][y+2]!=0:
+                            return y+2
 
         # check Diagonal
         for x in range(len(game_map[0]) - 3):
@@ -74,10 +99,10 @@ class AIPlayer(Player):
                     continue
                 elif game_map[y+1][x+1] == player_num and game_map[y+2][x+2] == player_num :
 
-                    if y+3<5 and x+3>7:
+                    if y+3<5 and x+3<7:
                         if game_map[y+4][x+3]!=0 and game_map[y+3][x+3]==0 :
                             return x+3
-                        
+
 
                     if y+3==5 and x+3<7:
                         if game_map[y+3][x+3]==0:
@@ -86,8 +111,8 @@ class AIPlayer(Player):
                     if y-1>=0 and x-1>=0:
                         if game_map[y][x-1]!=0 and game_map [y-1][x-1]==0:
                             return x-1
-                          
-                                        
+
+
         for x in range(3, len(game_map[0])):
             for y in range(len(game_map) - 2):
                 if game_map[y][x] != player_num:
@@ -98,16 +123,153 @@ class AIPlayer(Player):
                     if y+3<5 and x-3>=0:
                         if game_map[y+4][x-3]!=0 and game_map[y+3][x-3]==0 :
                             return x-3
-                        
+
 
                     elif y+3==5 and x-3>=0:
                         if game_map[y+3][x-3]==0:
                             return x-3
 
+                    if y-1>=0 and x+1<=6:
+                        if game_map[y][x+1]!=0 and game_map [y-1][x+1]==0:
+                            return x+1
+
+
+
+
+
+
+
+
+
+        return -1
+
+
+
+
+
+
+    def win_check_3(self,game_map,player_num,Opponent):
+        if player_num==Opponent:
+            opp=self.number
+        else :
+            opp=Opponent
+        map2=[[0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0]]
+
+        for v in range(len(game_map)):
+            for y in range(len(game_map[0])):
+                map2[v][y]=game_map[v][y]
+
+        #vertical
+        for x in range(len(game_map[0])):
+
+            for y in range(len(game_map) -1):
+                if game_map[y][x] != player_num:
+                    continue
+                elif game_map[y+1][x] == player_num  :
+
+
+
+                    if  y-1>0:
+
+                        if game_map[y-1][x]==0 and self.win_check_AI(map2,Opponent,game_map,x)==0 and self.win_check_AI(map2,self.number,game_map,x)==0 :
+
+                            return x
+
+
+
+
+
+
+
+
+        #horizantal
+
+        for x in range(len(game_map)):
+
+            for y in range(len(game_map[0]) -2):
+
+                if game_map[x][y] != player_num:
+                    continue
+                elif game_map[x][y+1] == player_num :
+                    if y-1>0  and y+2<7:
+
+                        if x==5 and game_map[x][y-1]==0 and (game_map[x][y+2]!=opp or game_map[x][y-2]!=opp):
+                            if self.win_check_AI(map2,Opponent,game_map,y-1)==0 and self.win_check_AI(map2,self.number,game_map,y-1)==0:
+
+                                return y-1
+
+
+                        elif x<5 and game_map[x+1][y-1]!=0 and game_map[x][y-1]==0 and (game_map[x][y+2]!=opp or game_map[x][y-2]!=opp):
+                            if self.win_check_AI(map2,Opponent,game_map,y-1)==0 and self.win_check_AI(map2,self.number,game_map,y-1)==0:
+
+
+                                return y-1
+
+
+                    if y+2<6 and y-1>-1:
+
+                        if x==5 and game_map[x][y+2] == 0 and (game_map[x][y+3]!=opp or game_map[x][y-1]!=opp):
+                            if self.win_check_AI(map2,Opponent,game_map,y+2)==0 and self.win_check_AI(map2,self.number,game_map,y+2)==0:
+                                return y+2
+
+                        elif x<5 and game_map[x+1][y+2]!=0 and game_map[x][y+2]==0 and (game_map[x][y-1]!=opp or game_map[x][y+3]!=opp):
+                            if self.win_check_AI(map2,Opponent,game_map,y+1)==0 and self.win_check_AI(map2,self.number,game_map,y+1)==0:
+                                return y+2
+
+
+                                # check Diagonal
+        for x in range(len(game_map[0]) - 3):
+            for y in range(len(game_map) - 2):
+                if game_map[y][x] != player_num:
+                    continue
+                elif game_map[y+1][x+1] == player_num :
+
+                    if y+2<5 and x+2<7:
+
+                        if game_map[y+3][x+2]!=0 and game_map[y+2][x+2]==0 :
+                            if self.win_check_AI(map2,Opponent,game_map,x+2)==0 and self.win_check_AI(map2,self.number,game_map,x+2)==0:
+                                return x+2
+
+
+                    if y+2==5 and x+2<7:
+                        if game_map[y+2][x+2]==0:
+                            if self.win_check_AI(map2,Opponent,game_map,x+2)==0 and self.win_check_AI(map2,self.number,game_map,x+2)==0:
+                                return x+2
+
                     if y-1>=0 and x-1>=0:
                         if game_map[y][x-1]!=0 and game_map [y-1][x-1]==0:
-                            return x-1                                           
-                                                   
+                            if self.win_check_AI(map2,Opponent,game_map,x-1)==0 and self.win_check_AI(map2,self.number,game_map,x-1)==0:
+
+                                return x-1
+
+
+        for x in range(3, len(game_map[0])):
+            for y in range(len(game_map) - 2):
+                if game_map[y][x] != player_num:
+                    continue
+                elif game_map[y+1][x-1] == player_num  :
+
+
+                    if y+2<5 and x-2>=0:
+                        if game_map[y+3][x-2]!=0 and game_map[y+2][x-2]==0 :
+                            if self.win_check_AI(map2,Opponent,game_map,x-2)==0 and self.win_check_AI(map2,self.number,game_map,x-2)==0:
+                                return x-2
+
+
+                    elif y+2==5 and x-2>=0:
+                        if game_map[y+2][x-2]==0:
+                            if self.win_check_AI(map2,Opponent,game_map,x-2)==0 and self.win_check_AI(map2,self.number,game_map,x-2)==0:
+                                return x-2
+
+                    if y-1>=0 and x+1<6:
+                        if game_map[y][x+1]!=0 and game_map [y-1][x+1]==0:
+                            if self.win_check_AI(map2,Opponent,game_map,x+1)==0 and self.win_check_AI(map2,self.number,game_map,x+1)==0:
+                                return x+1
 
 
 
@@ -116,28 +278,47 @@ class AIPlayer(Player):
 
 
 
-        return 0  
 
-        
+        return -1
+
+
+
 
 
     def win_check_AI(self,map,player_num,game_map1,column_num):
         flag=0
+
 
         if game_map1[0][column_num] == 0:
             for c in range(5,0,-1):
                 if game_map1[c-1][column_num]==0 and game_map1[c][column_num]==0 :
                     flag=1
                     break
-            
+
         else :
+
             return 1
         if flag==0:
             return 0
-            
-        map[c-1][column_num]=player_num           
 
-        gm = map
+
+        map[c-1][column_num]=player_num
+
+
+        gm=[[0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0]]
+        for x in range(len(map)):
+            for y in range(len(map[0])):
+                gm[x][y]=map[x][y]
+
+
+
+
+
 
         # check horizontal
         for row in gm:
@@ -145,6 +326,7 @@ class AIPlayer(Player):
                 if row[i] != player_num:
                     continue
                 elif row[i+1] == player_num and row[i+2] == player_num and row[i+3] == player_num:
+
                     return 1
 
         # check vertical
@@ -171,7 +353,6 @@ class AIPlayer(Player):
                     return 1
 
         return 0
-        
 
 
 
@@ -183,19 +364,20 @@ class AIPlayer(Player):
 
 
 
-            
+
+
 
     def get_drop_input(self, game_map):
 
         if self.number==1:
             Opponent=2
-            
+
         else :
             Opponent=1
 
 
-        print(Opponent)
-        print(self.number)    
+
+
 
 
 
@@ -204,61 +386,113 @@ class AIPlayer(Player):
         res1=self.check_Opponentـwin(game_map,self.number)
         res=self.check_Opponentـwin(game_map,Opponent)
 
-        if res1!=0:
+        if res1!=-1:
+
             return res1
 
 
-        if res!=0:
+        if res!=-1:
+
+
             return res
-
-                                       
-                    
-
-
-
-
-
-
-
-
-
-
-              
- 
-#3
+        x=0
+        y=0
 
         map2=[[0, 0, 0, 0, 0, 0, 0],
-                         [0, 0, 0, 0, 0, 0, 0],
-                         [0, 0, 0, 0, 0, 0, 0],
-                         [0, 0, 0, 0, 0, 0, 0],
-                         [0, 0, 0, 0, 0, 0, 0],
-                         [0, 0, 0, 0, 0, 0, 0]] 
+              [0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0]]
+
+        for v in range(len(game_map)):
+            for y in range(len(game_map[0])):
+                map2[v][y]=game_map[v][y]
+
+
+
+        res3=self.win_check_3(game_map,self.number,Opponent)
+        res4=self.win_check_3(game_map,Opponent,Opponent)
+
+        if res3!=-1:
+
+            return res3
+
+
+
+
+        if res4!=-1:
+
+            return res4
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        #3
+
+
         for x in range(len(game_map)):
             for y in range(len(game_map[0])):
                 map2[x][y]=game_map[x][y]
-                                    
-        
 
-        
+
+
+
+
+
+
+
+
+
         check_list=[3,4,2,5,1,0,6]
-        for x in range(len(check_list)):
-            print("aaavvvvvvvaaaaaaal")
-            print(x)
-            if self.win_check_AI(map2,1,game_map,check_list[x])==0:
-                return check_list[x]
-           
-            else :
-                print("pppppppppppa")
-
-        for x in range(len(check_list)):
-            
-            if game_map[0][check_list[x]]==0:
-                print(check_list[x])
-                print("gfgfg")
-                return 0
-             
-        return 1    
-            
+        x=0
+        y=0
 
 
-   
+
+        for c in range(len(check_list)):
+
+
+            if self.win_check_AI(map2,Opponent,game_map,check_list[c])==0 and self.win_check_AI(map2,self.number,game_map,check_list[c])==0:
+
+
+                return check_list[c]
+
+            for x in range(len(game_map)):
+                for y in range(len(game_map[0])):
+                    map2[x][y]=game_map[x][y]
+
+
+
+
+
+
+        for b in range(len(check_list)):
+
+
+            if game_map[0][check_list[b]]==0:
+                print(self.number)
+
+
+                return check_list[b]
+
+        return 1
+
+
+
